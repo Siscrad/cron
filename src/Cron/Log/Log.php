e1873e8806edd63448843e5138bdc13c36ac20aa
Heiko Griga <hgriga@teilehaber.de>
2018-04-03 15:33:37 +0200

    #master: test

--- src/Cron/Log/Log.php
+++ src/Cron/Log/Log.php
@@ -60,9 +60,9 @@ class Log
      * Log constructor.
      *
      * @param string  $file
-     * @param \mysqli $connection
+     * @param Database $connection
      */
-    public function  __construct(string $file, \mysqli $connection)
+    public function  __construct(string $file, Database $connection)
     {
         $this->file = $file;
         $this->connection = $connection;
